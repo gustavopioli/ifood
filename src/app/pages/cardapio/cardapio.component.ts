52835c85010f269d2107d7b7eb3538678653a76d
gustavopioli <gustavopiolibertolini3@gmail.com>
2025-05-20 14:40:41 -0300

    criacao primeiro card

--- src/app/pages/cardapio/cardapio.component.ts
+++ src/app/pages/cardapio/cardapio.component.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-cardapio',
-  imports: [],
+  imports: [CommonModule],
+  standalone: true,
   templateUrl: './cardapio.component.html',
-  styleUrl: './cardapio.component.scss'
+  styleUrl: './cardapio.component.scss',
 })
 export class CardapioComponent {
-
+  public headerButtons = [
+    {
+      name: 'Offers',
+      link: '/offers',
+    },
+    {
+      name: 'Burgers',
+      link: '/burgers',
+    },
+    {
+      name: 'Fries',
+      link: '/fries',
+    },
+    {
+      name: 'Snacks Salads',
+      link: '/snacks-salads',
+    },
+    {
+      name: 'Cold Drinks',
+      link: '/cold-drinks',
+    },
+    {
+      name: 'Happy Meal',
+      link: '/happy-meal',
+    },
+    {
+      name: 'Desserts',
+      link: '/desserts',
+    },
+    {
+      name: 'Hot Drinks',
+      link: '/hot-drinks',
+    },
+    {
+      name: 'Sauces',
+      link: '/sauces',
+    },
+    {
+      name: 'Orbit',
+      link: '/orbit',
+    },
+  ];
 }
